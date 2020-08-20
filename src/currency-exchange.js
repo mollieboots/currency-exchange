feeb297685df1aadd959dabb72adc3dc1d358588
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-08-20 10:06:47 -0700

    add class CurrencyExchange

--- src/currency-exchange.js
+++ src/currency-exchange.js
@@ -1,14 +1,15 @@
 export class CurrencyExchange {
     async getConversionRate() {
         try {
-            let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+            let response = await fetch(
+                `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+            );
             let jsonifiedResponse;
-            if (response.ok && response.status == 200) {
+            if (response.ok && response.status === 200) {
                 jsonifiedResponse = await response.json();
             } else {
                 jsonifiedResponse = false;
             }
-            console.log(jsonifiedResponse);
             return jsonifiedResponse;
         } catch (error) {
             return false;
