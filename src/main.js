8370711c5185aee5e9ede4b5a15abd458e698cc6
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-08-20 10:09:09 -0700

    add user interface logic to call API and take user inputs for conversion

--- src/main.js
+++ src/main.js
@@ -2,12 +2,29 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import $ from "jquery";
-import { CurrencyExchange } from './../src/currency-exchange.js'
+import { CurrencyExchange } from "./currency-exchange.js";
+
 
 $(document).ready(function () {
-    (async () => {
-        let currencyExchange = new CurrencyExchange();
-        const response = await currencyExchange.getConversionRate();
-        getElements(response);
-    })();
+    $("#amountSubmit").click(function (event) {
+        event.preventDefault();
+        const amount = $("#amount").val();
+        const currency = $("#currency").val();
+        $("#amount").val("");
+        (async () => {
+            let currencyExchange = new CurrencyExchange();
+            const response = await currencyExchange.getConversionRate();
+            getElements(response);
+        })();
+        
+        function getElements(response) {
+            if (response) {
+                $(".showResults").text(`You would have ${response.conversion_rates[currency] * amount} ${currency}`);
+            } else {
+                $(".showResults").text(`There was an error handling your request.`);
+                $(".showErrors").text(`Please check your inputs and try again!`);
+            }
+        }
+    });
+    
 });
